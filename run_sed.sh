a23c36dd3d6e7b3e1c0d7014f148573c990eb5b5
Adam Poulemanos <89049923+bashandbone@users.noreply.github.com>
2026-03-24 06:59:37 -0400

    Delete run_sed.sh

--- run_sed.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-sed -i 's/issues = validator.validate_file(test_file)/issues, _, _ = validator.validate_file(test_file)/g' tests/test_validator.py
-sed -i 's/issues = validator.validate_file(test_file)/issues, _, _ = validator.validate_file(test_file)/g' tests/test_integration.py
